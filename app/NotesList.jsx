aa2cb2c785f64c4a989fd49d06a009fcd078cb7e
fralnz <francesco@fragiolanza.eu>
2024-07-08 12:21:30 +0200

    visible custom notes

--- app/NotesList.jsx
+++ app/NotesList.jsx
@@ -8,8 +8,11 @@ import { getAllKeys } from "./utils/storageTools"; // Ensure this returns a prom
 import { transformDate, sortDates } from "./utils/dateTools";
 
 const NotesList = () => {
-  const [keys, setKeys] = useState([]);
+  const [dateKeys, setDateKeys] = useState([]);
+  const [customKeys, setCustomKeys] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [visibleKeys, setVisibleKeys] = useState([]);
+  const [isDate, setIsDate] = useState(false);
 
   const toggleModal = () => {
     setModalVisible(!modalVisible);
@@ -18,41 +21,57 @@ const NotesList = () => {
   useEffect(() => {
     const fetchKeys = async () => {
       const keysArray = await getAllKeys();
-      setKeys(sortDates(keysArray));
+      const { validDates, invalidDates } = sortDates(keysArray);
+      setDateKeys(validDates);
+      setCustomKeys(invalidDates);
     };
 
     fetchKeys();
   }, []);
 
   return (
-      <SafeAreaView
-          style={[styleAndroid.droidSafeArea, styleNotesList.notesPage]}
-      >
-        <CustomSwitch
-            switchLeftText={"Daily Notes"}
-            switchLeftTextStyle={{
-              color: "black",
-              fontSize: 16,
-              fontWeight: "600",
-            }}
-            switchRightText={"Custom Notes"}
-            switchRightTextStyle={{ color: "white", fontSize: 16, fontWeight: "600" }}
-            buttonWidth={40}
-            switchWidth={200}
-            buttonPadding={2}
-            switchBackgroundColor={"#0070F2"}
-            onSwitchBackgroundColor={"#EAECEF"}
-        />
-        <Button title="Show modal" onPress={toggleModal} />
-        <NewNoteModal isVisible={modalVisible} toggleModal={toggleModal} />
-        {keys.length > 0 ? (
-            keys.map((key, index) => (
-                <Text key={index}>{transformDate(key, "DD-MM-YYYY")}</Text>
-            ))
-        ) : (
-            <Text>No notes found</Text>
-        )}
-      </SafeAreaView>
+    <SafeAreaView
+      style={[styleAndroid.droidSafeArea, styleNotesList.notesPage]}
+    >
+      <CustomSwitch
+        switchLeftText={"Daily Notes"}
+        switchLeftTextStyle={{
+          color: "black",
+          fontSize: 16,
+          fontWeight: "600",
+        }}
+        switchRightText={"Custom Notes"}
+        switchRightTextStyle={{
+          color: "white",
+          fontSize: 16,
+          fontWeight: "600",
+        }}
+        buttonWidth={40}
+        switchWidth={200}
+        buttonPadding={2}
+        switchBackgroundColor={"#0070F2"}
+        onSwitchBackgroundColor={"#EAECEF"}
+        onSwitch={() => {
+          setVisibleKeys(dateKeys);
+          setIsDate(true);
+        }}
+        onSwitchReverse={() => {
+          setVisibleKeys(customKeys);
+          setIsDate(false);
+        }}
+      />
+      <Button title="Show modal" onPress={toggleModal} />
+      <NewNoteModal isVisible={modalVisible} toggleModal={toggleModal} />
+      {visibleKeys.length > 0 ? (
+        visibleKeys.map((key, index) => (
+          <Text key={index}>
+            {isDate ? transformDate(key, "DD-MM-YYYY") : key}
+          </Text>
+        ))
+      ) : (
+        <Text>No notes found</Text>
+      )}
+    </SafeAreaView>
   );
 };
 
--- app/utils/dateTools.js
+++ app/utils/dateTools.js
@@ -72,25 +72,37 @@ export const transformDate = (dateString, format) => {
 };
 
 export const sortDates = (arr) => {
-  // Filter out non-date elements and convert valid dates to Date objects
-  const validDates = arr.filter((dateStr) => {
+  // Initialize arrays for valid and invalid dates
+  const validDates = [];
+  const invalidDates = [];
+
+  // Process each element in the input array
+  arr.forEach((dateStr) => {
     // Use a regex pattern to match YYYY-MM-DD format
     if (/^\d{4}-\d{2}-\d{2}$/.test(dateStr)) {
       // Try creating a Date object from the string
       const dateObj = new Date(dateStr);
       // Check if the Date object is valid
-      return !isNaN(dateObj.getTime());
+      if (!isNaN(dateObj.getTime())) {
+        validDates.push(dateStr);
+      } else {
+        invalidDates.push(dateStr);
+      }
+    } else {
+      invalidDates.push(dateStr);
     }
-    return false;
   });
 
-  // Sort the Date objects
+  // Sort the valid date strings
   validDates.sort((dateStr1, dateStr2) => {
     const date1 = new Date(dateStr1);
     const date2 = new Date(dateStr2);
     return date1 - date2;
   });
 
-  // Return sorted date strings in YYYY-MM-DD format
-  return validDates;
+  // Return an object with valid dates and invalid dates
+  return {
+    validDates,
+    invalidDates,
+  };
 };
