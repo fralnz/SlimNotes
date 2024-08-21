4b3b64f00319a7e9605c0fd05515dc81a3130cea
fralnz <francesco@fragiolanza.eu>
2024-08-21 11:04:12 +0200

    Added SwipeableNote.jsx component

--- app/NotesList.jsx
+++ app/NotesList.jsx
@@ -3,18 +3,15 @@ import { Text, SafeAreaView, Pressable, Alert, Animated } from "react-native";
 import styleAndroid from "./style/styleAndroid";
 import styleNotesList from "./style/styleNotesList";
 import NewNoteModal from "./components/NewNoteModal";
-import { getAllKeys, removeValue } from "./hooks/storage.hooks";
-import { transformDate, sortDates } from "./hooks/date.hooks";
-import { useRouter } from "expo-router";
+import { getAllKeys } from "./hooks/storage.hooks";
+import { sortDates } from "./hooks/date.hooks";
 import BackHeader from "./components/BackHeader";
-import { useNoteContext } from "./hooks/notes.hook";
 import { FlashList } from "@shopify/flash-list";
 import { useIsFocused } from "@react-navigation/native";
-import Swipeable from "react-native-gesture-handler/Swipeable";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { DeleteIcon } from "./components/icons/DeleteIcon";
 import styleNoteEdit from "./style/styleNoteEdit";
 import NotesSwitch from "./components/NotesSwitch";
+import SwipeableNote from "./components/SwipeableNote";
 
 const NotesList = () => {
   const [dateKeys, setDateKeys] = useState([]);
@@ -22,8 +19,6 @@ const NotesList = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [visibleKeys, setVisibleKeys] = useState([]);
   const [isDate, setIsDate] = useState(false);
-  const router = useRouter();
-  const { dateFormat } = useNoteContext();
   const isFocused = useIsFocused();
 
   const toggleModal = () => {
@@ -47,52 +42,6 @@ const NotesList = () => {
     setVisibleKeys(isDate ? dateKeys : customKeys);
   }, [customKeys, dateKeys]);
 
-  const handleDelete = async (key) => {
-    Alert.alert("Delete", `Are you sure you want to delete ${key}?`, [
-      {
-        text: "Cancel",
-        style: "cancel",
-      },
-      {
-        text: "Delete",
-        style: "destructive",
-        onPress: async () => {
-          await removeValue(key);
-
-          // Filter out the deleted key from both dateKeys and customKeys
-          const updatedDateKeys = dateKeys.filter((dateKey) => dateKey !== key);
-          const updatedCustomKeys = customKeys.filter(
-            (customKey) => customKey !== key,
-          );
-
-          setDateKeys(updatedDateKeys);
-          setCustomKeys(updatedCustomKeys);
-
-          // Update visible keys accordingly
-          setVisibleKeys(isDate ? updatedDateKeys : updatedCustomKeys);
-        },
-      },
-    ]);
-  };
-
-  const renderLeftActions = (progress, dragX, key) => {
-    const translateX = dragX.interpolate({
-      inputRange: [0, 100],
-      outputRange: [-100, 0],
-      extrapolate: "clamp",
-    });
-
-    return (
-      <Animated.View
-        style={[styleNoteEdit.deleteAnimation, { transform: [{ translateX }] }]}
-      >
-        <Pressable onPress={() => handleDelete(key)}>
-          <DeleteIcon />
-        </Pressable>
-      </Animated.View>
-    );
-  };
-
   return (
     <SafeAreaView
       style={[styleAndroid.droidSafeArea, styleNotesList.notesPage]}
@@ -109,24 +58,15 @@ const NotesList = () => {
           <FlashList
             data={visibleKeys}
             renderItem={({ item }) => (
-              <Swipeable
-                renderLeftActions={(progress, dragX) =>
-                  renderLeftActions(progress, dragX, item)
-                }
-              >
-                <Pressable
-                  onPress={() =>
-                    router.push({
-                      pathname: "/",
-                      params: { key: item },
-                    })
-                  }
-                >
-                  <Text style={styleNotesList.list}>
-                    {isDate ? transformDate(item, dateFormat) : item}
-                  </Text>
-                </Pressable>
-              </Swipeable>
+              <SwipeableNote
+                item={item}
+                dateKeys={dateKeys}
+                customKeys={customKeys}
+                setCustomKeys={setCustomKeys}
+                setDateKeys={setDateKeys}
+                setVisibleKeys={setVisibleKeys}
+                isDate={isDate}
+              />
             )}
             estimatedItemSize={200}
           />
---
+++ app/components/SwipeableNote.jsx
@@ -0,0 +1,82 @@
+import { Alert, Animated, Pressable, Text } from "react-native";
+import styleNotesList from "../style/styleNotesList";
+import { transformDate } from "../hooks/date.hooks";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import React from "react";
+import { removeValue } from "../hooks/storage.hooks";
+import styleNoteEdit from "../style/styleNoteEdit";
+import { DeleteIcon } from "./icons/DeleteIcon";
+import {useNoteContext} from "../hooks/notes.hook";
+
+const SwipeableNote = ({ item, dateKeys, customKeys, setDateKeys, setCustomKeys, setVisibleKeys, isDate }) => {
+  const { dateFormat } = useNoteContext();
+
+  const handleDelete = async (key) => {
+    Alert.alert("Delete", `Are you sure you want to delete ${key}?`, [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: async () => {
+          await removeValue(key);
+
+          // Filter out the deleted key from both dateKeys and customKeys
+          const updatedDateKeys = dateKeys.filter((dateKey) => dateKey !== key);
+          const updatedCustomKeys = customKeys.filter(
+            (customKey) => customKey !== key,
+          );
+
+          setDateKeys(updatedDateKeys);
+          setCustomKeys(updatedCustomKeys);
+
+          // Update visible keys accordingly
+          setVisibleKeys(isDate ? updatedDateKeys : updatedCustomKeys);
+        },
+      },
+    ]);
+  };
+
+  const renderLeftActions = (progress, dragX, key) => {
+    const translateX = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [-100, 0],
+      extrapolate: "clamp",
+    });
+
+    return (
+      <Animated.View
+        style={[styleNoteEdit.deleteAnimation, { transform: [{ translateX }] }]}
+      >
+        <Pressable onPress={() => handleDelete(key)}>
+          <DeleteIcon />
+        </Pressable>
+      </Animated.View>
+    );
+  };
+
+  return (
+    <Swipeable
+      renderLeftActions={(progress, dragX) =>
+        renderLeftActions(progress, dragX, item)
+      }
+    >
+      <Pressable
+        onPress={() =>
+          router.push({
+            pathname: "/",
+            params: { key: item },
+          })
+        }
+      >
+        <Text style={styleNotesList.list}>
+          {isDate ? transformDate(item, dateFormat) : item}
+        </Text>
+      </Pressable>
+    </Swipeable>
+  );
+};
+
+export default SwipeableNote;
